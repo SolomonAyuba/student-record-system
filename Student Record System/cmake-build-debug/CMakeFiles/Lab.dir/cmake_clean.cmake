1a821c5baad37764933be5b18ced940e6b28e891
Solomon Ayuba <solomon.ayuba@miva.edu.ng>
2024-07-07 23:01:14 +0100

    Initiated a new branch to implement brainstorming session

---
+++ Student Record System/cmake-build-debug/CMakeFiles/Lab.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab.dir/SA.c.o"
+  "CMakeFiles/Lab.dir/SA.c.o.d"
+  "CMakeFiles/Lab.dir/main.c.o"
+  "CMakeFiles/Lab.dir/main.c.o.d"
+  "Lab"
+  "Lab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
